95470d257e1e2cf465d9f5fd406173f20549f508
ThotuB <59542225+ThotuB@users.noreply.github.com>
2023-04-28 20:09:34 +0300

    done with poli

---
+++ year-IV/semester-2/EXPS - Expert Systems/PROJECT/app/analysis/factors.py
@@ -0,0 +1,346 @@
+FACTORS = {
+    "REGION": {
+        "description": "Region",
+        "values": {
+            "1": "Northeast",
+            "2": "Midwest",
+            "3": "South",
+            "4": "West"
+        }
+    },
+    "AGE31X": {
+        "description": "Age",
+    },
+    "GENDER": {
+        "desription": "Gender",
+        "values": {  # TODO
+            "0.0": "Male",
+            "1.0": "Female"
+        }
+    },
+    "RACE3": {
+        "description": "Race",
+        "values": {
+            "0.0": "White",
+            "1.0": "Black",
+        },
+    },
+    "MARRY31X": {
+        "description": "Marital Status",
+        "values": {
+            "1": "Married",
+            "2": "Widowed",
+            "3": "Separated",
+            "4": "Divorced",
+            "5": "Never Married",
+            "6": "Under 16 - Inaplicable",
+            "7": "Married in round",
+            "8": "Widowed in round",
+            "9": "Separated in round",
+            "10": "Divorced in round",
+        }
+    },
+    "EDRECODE": {
+        "description": "Education",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Less than 9th grade",
+            "2": "9th to 12th grade, no high school diploma or GED",
+            "13": "High school graduate/GED",
+            "14": "Beyond high school, associate degree or college without 4 year degree",
+            "15": "Bachelor's degree, 4 year college degree",
+            "16": "Master's degree, doctorate, professional degree",
+        }
+    },
+    "FTSTU31X": {
+        "description": "Student Status",
+        "values": {  # TODO
+            "-1": "Inaplicable",
+            "1": "Full-time student",
+            "2": "Part-time student",
+            "3": "Not a student",
+        }
+    },
+    "ACTDTY31": {
+        "description": "Military Service",
+        "values": {
+            "1": "Yes",
+            "2": "No",
+            "3": "Under 16 - Inaplicable",
+            "4": "Over 59 - Inaplicable",
+        }
+    },
+    "HONRDC31": {
+        "description": "Honorably Discharged",
+        "values": {
+            "1": "Yes",
+            "2": "No",
+            "3": "Under 16 - Inaplicable",
+            "4": "Active Duty",
+        }
+    },
+    "RTHLTH31": {
+        "description": "Perceived Health Status",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Excellent",
+            "2": "Very Good",
+            "3": "Good",
+            "4": "Fair",
+            "5": "Poor",
+        }
+    },
+    "MNHLTH31": {
+        "description": "Perceived Mental Health Status",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Excellent",
+            "2": "Very Good",
+            "3": "Good",
+            "4": "Fair",
+            "5": "Poor",
+        }
+    },
+    "HIBPDX": {
+        "description": "High Blood Pressure",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "CHDDX": {
+        "description": "Congestive Heart Disease",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "ANGIDX": {
+        "description": "Angina",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "MIDX": {
+        "description": "Heart Attack",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "OHRTDX": {
+        "description": "Other Heart Disease",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "STRKDX": {
+        "description": "Stroke",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "EMPHDX": {
+        "description": "Emphysema",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "CHBRON31": {
+        "description": "Chronic Bronchitis",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "CHOLDX": {
+        "description": "Cholesterol",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "CANCERDX": {
+        "description": "Cancer",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "DIABDX": {
+        "description": "Diabetes",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "JTPAIN31": {
+        "description": "Joint Pain",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "ARTHDX": {
+        "description": "Arthritis",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "ARTHTYPE": {
+        "description": "Arthritis Type",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Rheumatoid arthritis",
+            "2": "Osteoarthritis",
+            "3": "Non-specific arthritis",
+        }
+    },
+    "ASTHDX": {
+        "description": "Asthma",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "ADHDADDX": {
+        "description": "ADHD/ADD",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "PREGNT31": {
+        "description": "Pregnant",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "WLKLIM31": {
+        "description": "Walking Limitation",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "ACTLIM31": {
+        "description": "Activities Limitation",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "SOCLIM31": {
+        "description": "Social Limitation",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "COGLIM31": {
+        "description": "Cognitive Limitation",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "DFHEAR42": {
+        "description": "Difficulty Hearing",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "DFSEE42": {
+        "description": "Difficulty Seeing",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "ADSMOK42": {
+        "description": "",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Yes",
+            "2": "No",
+        }
+    },
+    "PCS42": {
+        "description": "Physical Condition Score",
+    },
+    "MCS42": {
+        "description": "Mental Condition Score",
+    },
+    "K6SUM42": {
+        "description": "Kessler Psychological Distress Index Score",
+    },
+    "PHQ242": {
+        "description": "Patient Health Questionnaire Score",
+    },
+    "EMPST31": {
+        "description": "Employment Status",
+        "values": {
+            "-1": "Inaplicable",
+            "1": "Employed",
+            "2": "Job to return to",
+            "3": "Unemployed",
+            "4": "Unemployed",
+        }
+    },
+    "POVCAT15": {
+        "description": "Poverty Category",
+        "values": {
+            "1": "Poor",
+            "2": "Near poor",
+            "3": "Low income",
+            "4": "Middle income",
+            "5": "High income"
+        }
+    },
+    "INSCOV15": {
+        "description": "Insurance Coverage",
+        "values": {
+            "1": "Any private insurance",
+            "2": "Public insurance only",
+            "3": "Uninsured",
+        }
+    },
+    "INCOME_M": {
+        "description": "Income",
+    },
+    "HEALTHEXP": {
+        "description": "Health Expenditures",
+    },
+    "PERSONWT": {
+        "description": "Personal Net Worth",
+    }
+}
